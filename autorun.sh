0c06bf3d4f1313755894bfe6f7b36d0aafd57e41
Denis Julio <djms1234@gmail.com>
2024-12-04 08:46:03 -0300

    adds picom server start autorun

--- autorun.sh
+++ autorun.sh
@@ -6,6 +6,7 @@ run () {
   fi
 }
 
+run "picom"
 run "ulauncher" --hide-window --no-window-shadow
 run "redshift" -b 0.8 -l 18:41
 run "bash" -c "input-remapper-control --command stop-all && input-remapper-control --command autoload"
